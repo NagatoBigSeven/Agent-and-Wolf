166c4d8af1706054d51282dfff52f4a966441e34
NagatoBigSeven <zzmhkust@gmail.com>
2025-09-26 00:47:13 +0200

    add firestore support

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import os
 from dotenv import load_dotenv
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_community.document_loaders import TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
 from langchain.chains import create_history_aware_retriever, create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
-
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from google.cloud import firestore
+from langchain_google_firestore import FirestoreChatMessageHistory
 def create_database(db_name):
     root_dir = os.path.dirname(os.path.abspath(__file__))
     db_dir = os.path.join(root_dir, "db")
@@ -81,29 +82,6 @@ def chat():
         "\n\n"
         "{context}"
     )
-    """
-    chat_system_message = (
-        "你是支仓冻砂创作的轻小说《狼与香辛料》及其衍生作品的女主角赫萝。"
-        "你的外表是拥有狼耳与尾巴的15岁左右的少女，自称为“贤狼赫萝”，但实际上是神话中被尊为神明的巨狼，寄宿在帕斯罗村的麦子中带来长期丰收，因为不再被信仰而想要离开，在帕斯罗村的庆典中从帕斯罗村的仓库逃入罗伦斯马车上的麦子，与罗伦斯一同行商，想回到遥远北方的出生故乡“约伊兹森林”。"
-        "你跟自称“贤狼”相符的冷静老练言语，丰富的经验与智慧常常拯救罗伦斯。"
-        "你性格自大，但因为长期离开故乡因此有着孤独脆弱的一面。"
-        "你的第一人称词为“奴家/咱”（日语：わっち），第二人称词为“汝”（日语：ぬし），语助词则以“呗”（日语：～でありんす）作结，这种独特的口癖是受到花魁影响的风廓词。"
-        "你与罗伦斯共同遭遇了各种事情，途中虽然常常主导对话，但也有因为不了解现代知识而被驳倒的时候。"
-        "你喜欢美味的食物与酒，但似乎特别喜欢苹果及甜食。"
-        "你在追伊弗的时候，意外被罗伦斯发现害怕乘船。"
-        "你因为曾经被奉为神明而本能地喜欢帮助他人，但对方没有主动要求，你也不会去回应，常对于无法出一份力的自己感到有些自责。"
-        "你作为麦子守护的神明、有着能让天气变为能让麦子更快的生长的能力，只要是在正常的自然范围内的话就能做到。只要自己寄宿的麦子还在的话，就不会死，如果将最后一捆麦子里的麦粒吃掉，就能恢复原来的样子，但是本体是无法离开麦子的。"
-        "你对自己的美丽尾巴十分自豪，经常不懈怠地用梳子整理、清除跳蚤以及用高级的油保养，十分喜欢被别人赞美尾巴；如果别人糟蹋了你的尾巴，将会发生无法预知的严重后果。"
-        "由于种族的原因，你喜欢天体，对女性服装喋喋不休。"
-        "你喜欢罗伦斯，曾经向罗伦斯提出结束旅行，最终被说服，也回应了“咱如果喜欢上汝，汝可是会很麻烦的”。"
-        "你能够变身成为巨大体型的狼，拥有极强的战斗力、速度，但在狼形态下仍能说话。"
-        "\n\n"
-        "请根据以下检索到的上下文信息回答问题。如果上下文中没有相关信息，请回答“奴家也不清楚呗”。"
-        "请保持赫萝的说话风格和个性，回答要简洁明了。"
-        "\n\n"
-        "{context}"
-    )
-    """
     chat_prompt_template = ChatPromptTemplate.from_messages(
         [
             ("system", chat_system_message),
@@ -113,19 +91,23 @@ def chat():
     )
     stuff_documents_chain = create_stuff_documents_chain(chat_llm, chat_prompt_template)
     retrieval_chain = create_retrieval_chain(history_aware_retriever, stuff_documents_chain)
-    history = []
-    print("开始与赫萝聊天吧! 输入 ’退出‘ 以结束对话。")
+    # chat_history = []
+    PROJECT_ID = "langchain-de693"
+    SESSION_ID = "user_session_20250926"
+    COLLECTION_NAME = "chat_history"
+    client = firestore.Client(project=PROJECT_ID)
+    chat_history = FirestoreChatMessageHistory(session_id=SESSION_ID, collection=COLLECTION_NAME, client=client)
+    print("开始与贤狼赫萝聊天吧! 输入 ’退出‘ 以结束对话。")
     while True:
         query = input("您: ")
         if query == "退出":
             break
-        response = retrieval_chain.invoke({"input": query, "chat_history": history})
+        response = retrieval_chain.invoke({"input": query, "chat_history": chat_history.messages})
         print("赫萝: " + response["answer"])
-        history.append(HumanMessage(content=query))
-        history.append(AIMessage(content=response["answer"]))
+        chat_history.add_user_message(query)
+        chat_history.add_ai_message(response["answer"])
 def main():
     load_dotenv()
     chat()
-
 if __name__ == "__main__":
     main()
